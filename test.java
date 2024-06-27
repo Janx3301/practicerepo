f75af407b8b13a230e9a13dee2557fa1245593df
Janwin Joy <janwinjoy@protonmail.com>
2024-06-27 12:03:02 +0530

    test.java added

---
+++ test.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    void add(int a, int b) {
+        System.out.println(a + b);
+    }
+}
